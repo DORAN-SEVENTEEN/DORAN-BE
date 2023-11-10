dde387e491e92622e834a9a4ab2f8081ae805286
Gaeun Lee <rk243812@gmail.com>
2023-11-11 01:07:53 +0900

    config gradle 다시 복귀

--- src/main/java/com/seventeen/doran/DorandoranApplication.java
+++ src/main/java/com/seventeen/doran/DorandoranApplication.java
@@ -2,11 +2,6 @@ package com.seventeen.doran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.multipart.MultipartResolver;
-
-import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class DorandoranApplication {
--- src/main/java/com/seventeen/doran/config/WebConfig.java
+++ src/main/java/com/seventeen/doran/config/WebConfig.java
@@ -17,11 +17,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600);
     }
 
-    @Bean
-    public CommonsMultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩 설정
-        multipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024); // 파일당 업로드 크기 제한 (5MB)
-        return multipartResolver;
-    }
 }

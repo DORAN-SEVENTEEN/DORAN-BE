58f6a1346eff0ed72a9e13648a61da09af22dcb6
Gaeun Lee <rk243812@gmail.com>
2023-11-11 00:43:32 +0900

    config gradle 변경

--- src/main/java/com/seventeen/doran/DorandoranApplication.java
+++ src/main/java/com/seventeen/doran/DorandoranApplication.java
@@ -2,6 +2,11 @@ package com.seventeen.doran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartResolver;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class DorandoranApplication {
@@ -9,5 +14,4 @@ public class DorandoranApplication {
   public static void main(String[] args) {
     SpringApplication.run(DorandoranApplication.class, args);
   }
-
 }
--- src/main/java/com/seventeen/doran/config/WebConfig.java
+++ src/main/java/com/seventeen/doran/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.seventeen.doran.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩 설정
+        multipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024); // 파일당 업로드 크기 제한 (5MB)
+        return multipartResolver;
     }
 }
